22887313f5bfbe0f6e4876902d841983b9d79c8f
Muhammad Ahsan Kaleem <muhammad.ahsan.kaleem@gmail.com>
2019-05-23 11:33:02 +0500

    Add files via upload

---
+++ thompsons_lamp.ino
@@ -0,0 +1,38 @@
+int pushbutton = 2;
+int led = 13;
+float x = 7500;
+int state;
+float a;
+float b;
+void setup() {
+  pinMode(pushbutton, INPUT);
+  pinMode(led, OUTPUT);
+}
+
+void loop() {
+  state = digitalRead(pushbutton);
+  if (state == 1)
+  {
+    digitalWrite(led, HIGH);
+    if (x < 100)
+    {
+      a = x * 1000;
+      delayMicroseconds(a);
+    }
+    else
+    {
+      delay(x);
+    }
+    digitalWrite(led, LOW);
+    x = x / 2;
+    if (x < 100)
+    {
+      b = x * 1000;
+      delayMicroseconds(b);
+    }
+    else
+    {
+      delay(x);
+    }
+  }
+}
